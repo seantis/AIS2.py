263fa99ac1dab773e13c690d724ecbf9b6f48de8
David Salvisberg <david.salvisberg@seantis.ch>
2022-04-14 14:30:02 +0200

    Excludes types.py from coverage.

--- AIS/types.py
+++ AIS/types.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+AIS.py - A Python interface for the Swisscom All-in Signing Service.
+
+:copyright: (c) 2016 by Camptocamp
+:license: AGPLv3, see README and LICENSE for more details
+
+"""
+
 from typing import Protocol
 from typing import Union
 
